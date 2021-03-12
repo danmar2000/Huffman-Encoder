f2fb8164710c950b7f5d60e027099f3b21d73749
Daniel <markodanielm@gmail.com>
2021-03-12 22:14:15 +0200

    Add EncodeTable class

--- huffman.py
+++ huffman.py
@@ -1,14 +1,7 @@
-
-'''
-    Function which gets a list and return a set
-'''
-
-def list2set(my_list):
-    final_set = []
-    for element in my_list:
-        if element not in final_set:
-            final_set.append(element)
-    return final_set
+"""
+Huffman Encoder
+Author: Danmar and Lidi
+"""
 
 '''
     Function which gets a data and returns the frequency of each symbol
@@ -23,14 +16,29 @@ def create_freq_table(my_data):
         else:
             freq_table[symbol] += 1
     for symbol in freq_table.keys():
-        freq_table[symbol] = freq_table[symbol] / len(my_data) # total frequencies is 1
+        freq_table[symbol] = freq_table[symbol] / len(my_data)  # total frequencies is 1
     return freq_table
+
+
+'''
+    Function which gets a list and return a set
+'''
+
+
+def list2set(my_list):
+    final_set = []
+    for element in my_list:
+        if element not in final_set:
+            final_set.append(element)
+    return final_set
+
 
 '''
     Function which gets a data and frequency table and returns it as an heap.
 '''
 
-def create_priority_list(my_data, freq_table):
+
+def create_priority_list(freq_table):
     values = []
     while len(freq_table.keys()) > 1:
         first_min = min(freq_table, key=freq_table.get)
@@ -41,70 +49,61 @@ def create_priority_list(my_data, freq_table):
         second_min_value = freq_table[second_min]
         freq_table.pop(second_min)
 
-        freq_table[first_min+second_min] = first_min_value + second_min_value
+        freq_table[first_min + second_min] = first_min_value + second_min_value
         new_val = str(first_min) + str(second_min)
         values.append(first_min)
         values.append(second_min)
         values.append(new_val)
 
-    values.reverse() #up-down
+    values.reverse()  # up-down
     return list2set(values[1:])
 
 
-'''
-    Function which gets a data and priority list and based on that creates encode_table
-'''
+class EncodeTable:
+    def __init__(self, input_string):
+        self._input_string = input_string
 
-def create_encode_table(my_data, priority_list):
-    encode_table = {}
-    for i in range(len(priority_list)):
-        if i%2 == 0:
-            encode_table[priority_list[i]] = 0
-        else:
-            encode_table[priority_list[i]] = 1
-    final_encode_table = {}
-    for symbol in [i for i in priority_list if len(i) == 1]:
-        for element in encode_table:
-            if symbol in element and symbol in final_encode_table:
-                final_encode_table[symbol] += str(encode_table[element])
-            if symbol in element and symbol not in final_encode_table:
-                final_encode_table[symbol] = str(encode_table[element])
+        freq_table = create_freq_table(input_string)
+        priority_list = create_priority_list(freq_table)
+        self._initialize_encode_table(priority_list)
 
-    return final_encode_table
+    def encode(self):
+        translation = ''
+        for char in self._input_string:
+            translation += str(self._table[char])
+        return translation
 
-'''
-    Function which gets a data and encode_table and based on that translates the data to binary code
-'''
+    def __str__(self):
+        lines = []
 
-def encode(my_data, encode_table):
-    translation = ''
-    for symbol in my_data:
-        translation += str(encode_table[symbol])
-    return translation
+        for symbol, value in self._table.items():
+            symbol_name = "space" if symbol == ' ' else symbol
 
+            line = f"{symbol_name}: {value}"
+            lines.append(line)
 
-'''
-    Function which gets a data and returns it hoffman encoded
-'''
+        return '\n'.join(lines)
 
-def huffman(my_data):
-    freq_table = create_freq_table(my_data)
-    priority_list = create_priority_list(my_data, freq_table)
-    encode_table = create_encode_table(my_data, priority_list)
-    return(encode(my_data,encode_table))
+    def _initialize_encode_table(self, priority_list):
+        initial_encode_table = EncodeTable._generate_initial_table(priority_list)
 
+        self._table = {}
+        single_char_symbols = [symbol for symbol in priority_list if len(symbol) == 1]
 
-'''
-    Function which gets an encoder table and returns a format you can print on screen
-'''
+        for char in single_char_symbols:
+            for symbol in initial_encode_table:
+                if char in symbol:
+                    if char in self._table:
+                        self._table[char] += str(initial_encode_table[symbol])
+                    else:
+                        self._table[char] = str(initial_encode_table[symbol])
 
-
-def encoder2txt(encoder_table):
-    result = ''
-    for symbol in encoder_table.keys():
-        if symbol == ' ':
-            result += ('space: '+encoder_table[symbol]+'\n')
-        else:
-            result += (symbol +': '+encoder_table[symbol]+'\n')
-    return result
-
+    @staticmethod
+    def _generate_initial_table(priority_list):
+        encode_table = {}
+        for i in range(len(priority_list)):
+            if i % 2 == 0:
+                encode_table[priority_list[i]] = 0
+            else:
+                encode_table[priority_list[i]] = 1
+        return encode_table
--- screen.py
+++ screen.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, Entry, Button, Label
-from huffman import *
+from huffman import EncodeTable
 
 window = Tk()
 window.geometry("1000x700")
@@ -10,15 +10,16 @@ lbl_result = Label(window, font=('Tahoma', '12'), bg='ghost white')
 lbl_encoder = Label(window, font=('Tahoma', '14'), bg='ghost white')
 
 
-def start_huffman(my_data):
-    lbl_result['text'] = 'Binary Code: '+huffman(my_data) + '\n'
-    lbl_encoder['text'] = 'Encoding Table: ''\n'+encoder2txt(create_encode_table(my_data, create_priority_list(my_data, create_freq_table(my_data))))
+def start(my_data):
+    encoder = EncodeTable(my_data)
+    lbl_result['text'] = 'Binary Code: ' + encoder.encode() + '\n'
+    lbl_encoder['text'] = 'Encoding Table: ''\n' + str(encoder)
 
 
 lbl_title = Label(window, text='Huffman Encoder', font=('Tahoma', '36'), bg='ghost white')
 entry_text = Entry(window, font=('Tahoma', '24'), bg='sky blue')
 entry_text.insert(0, 'Enter the data')
-btn_cmd = Button(window, text='Encode', font=('Tahoma', '24'), command=lambda: start_huffman(entry_text.get()))
+btn_cmd = Button(window, text='Encode', font=('Tahoma', '24'), command=lambda: start(entry_text.get()))
 
 lbl_title.pack()
 entry_text.pack()
